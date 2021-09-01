51e9a724d14734c6f2a5ed8d05068fbdcfda23a1
shrvya <shrivyashetty@gmail.com>
2021-09-01 20:36:06 +0530

    added:stack and que operations

---
+++ StackOperation.java
@@ -0,0 +1,17 @@
+package stack;
+
+/*
+ * this class implements stack using linked list
+ * head,current are pointers to nodes of linked list
+ * 
+ */
+
+public class StackOperation<T> {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Stack opertaion");
+
+	}
+}
