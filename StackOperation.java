0f92affa3f7d4263ca0078c1289a99b8b7d2c0fd
shrvya <shrivyashetty@gmail.com>
2021-09-01 20:41:33 +0530

    added:push data to stack

--- StackOperation.java
+++ StackOperation.java
@@ -8,10 +8,50 @@ package stack;
 
 public class StackOperation<T> {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Stack opertaion");
+	Node head;
 
+	/*
+	 * this class is used to create nodes for linked list data is the key value of
+	 * linked list next points to next node in linked list
+	 */
+	class Node<T> {
+		T data;
+		Node<T> next;
+
+		/*
+		 * this is a constructor of class Node this constructor will create node with
+		 * data of type T and next will be null
+		 */
+		Node(T d) {
+			data = d;
+			next = null;
+		}
+
+	}
+
+	/*
+	 * this is push method of stack the last pushed element is at the top this is
+	 * same like adding nodes at front in linked list data is the key value of the
+	 * node
+	 */
+	public void push(T data) {
+
+		Node<T> newnode = new Node<T>(data);
+		newnode.next = head;
+		head = newnode;
+	}
+	/*
+	 * this method will print the linked list tempnode , a node is considered it
+	 * acts like a iterator and iterates till last node this method will display
+	 * content of linked list
+	 */
+
+	public void print() {
+		Node<T> tempnode = head;
+		while (tempnode.next != null) {
+			System.out.print(tempnode.data + "-->");
+			tempnode = tempnode.next;
+		}
+		System.out.println(tempnode.data);
 	}
 }
---
+++ StackTest.java
@@ -0,0 +1,28 @@
+package stack;
+
+/*
+ * this  class has the main method 
+ * it calls the methods in StackOperation class
+ */
+public class StackTest {
+
+	/*
+	 * this is the main method it will create object of Integer type for
+	 * StackOpertaion and call method push an print
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		StackOperation<Integer> linklist = new StackOperation<Integer>();
+		System.out.println("The Stack is :");
+		linklist.push(70);
+		linklist.push(30);
+		linklist.push(56);
+		linklist.print();
+	}
+
+}
+
+
+
+
